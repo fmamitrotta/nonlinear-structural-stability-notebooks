db6018ffee1199410618b85ac623a9d3b1904577
Francesco Mitrotta <fma.mitrotta@gmail.com>
2023-10-02 19:32:50 +0100

    Changed licese notice in notebooks and python files
    Former-commit-id: 4be3e94fe851678dbcac6a050cf1ede364dbb337

--- notebooks/resources/pynastran_utils.py
+++ notebooks/resources/pynastran_utils.py
@@ -1,19 +1,13 @@
 """
 This file is part of the GitHub repository nonlinear-structural-stability-notebooks, created by Francesco M. A. Mitrotta.
-Copyright (C) 2023 Francesco Mario Antonio Mitrotta
+Copyright (C) 2022 Francesco Mario Antonio Mitrotta
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
 import matplotlib.pyplot as plt
@@ -150,8 +144,8 @@ def read_nonlinear_buckling_load_from_f06(f06_filepath: str, op2_object: OP2) ->
 
     Returns
     -------
-    nonlinear_buckling_load: ndarray
-        array of nonlinear buckling loads calculated as  P + Delta P * alpha
+    nonlinear_buckling_load_vectors: ndarray
+        array of the nonlinear buckling load vectors, calculated as P + Delta P * alpha
     alphas: ndarray
         array of critical buckling factors, used to verify that absolute value is not greater than unity
     """
@@ -165,26 +159,18 @@ def read_nonlinear_buckling_load_from_f06(f06_filepath: str, op2_object: OP2) ->
     # Find valid subcases in the OP2 object (subcases with list of load steps)
     valid_subcase_ids = [subcase_id for subcase_id in op2_object.load_vectors if
                          hasattr(op2_object.load_vectors[subcase_id], 'lftsfqs')]
-    # Initialize final load of previous subcase
-    final_load_previous_subcase = 0
-    # Initialize list of nonlinear buckling loads
-    nonlinear_buckling_loads = np.empty(len(alphas))
+    # Initialize list of nonlinear buckling load vectors
+    nonlinear_buckling_load_vectors = np.empty((len(alphas)) + np.shape(op2_object.load_vectors[valid_subcase_ids[0]].data[-1, :, :]))
     # Iterate through the valid subcases
     for i, subcase_id in enumerate(valid_subcase_ids):
-        # Calculate the magnitude of the total applied laod at end of current subcase
-        final_load = np.linalg.norm(np.apply_along_axis(np.sum, 0, op2_object.load_vectors[subcase_id].data[-1, :, 0:3]))
-        # Find incremental load applied in current subcase with respect to previous subcase
-        applied_load = final_load - final_load_previous_subcase
-        # Store load steps of current subcase
-        load_steps = op2_object.load_vectors[subcase_id].lftsfqs
-        # Find last load increment of current subcase
-        last_load_increment = applied_load * (load_steps[-1] - load_steps[-2])
+        # Find the final load vector of current subcase
+        final_load_vector = op2_object.load_vectors[subcase_id].data[-1, :, :]
+        # Find last increment vector of current subcase
+        last_increment_vector = final_load_vector - op2_object.load_vectors[subcase_id].data[-2, :, :]
         # Calculate nonlinear buckling load as P+DeltaP*ALPHA
-        nonlinear_buckling_loads[i] = final_load + last_load_increment * alphas[i]
-        # Update final load of previous subcase
-        final_load_previous_subcase = final_load
+        nonlinear_buckling_load_vectors[i] = final_load_vector + last_increment_vector * alphas[i]
     # Return lists of nonlinear buckling loads and critical buckling factors
-    return nonlinear_buckling_loads, np.array(alphas)
+    return nonlinear_buckling_load_vectors, np.array(alphas)
 
 
 def read_kllrh_lowest_eigenvalues_from_f06(f06_filepath: str) -> ndarray:
